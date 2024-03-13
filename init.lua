2cd2faca64e23aebca99fdf1a780c72d43318d26
Peter Kemner <peter.kemner@student.hu.nl>
2024-03-13 23:37:04 +0100

    added keybindings

---
+++ init.lua
@@ -0,0 +1,33 @@
+vim.cmd("language en_US")
+vim.cmd("set number")
+vim.cmd("set relativenumber")
+vim.cmd("set mouse=a")
+vim.cmd("set autoindent")
+vim.cmd("set tabstop=4")
+vim.cmd("set shiftwidth=4")
+vim.cmd("set smarttab")
+vim.cmd("set softtabstop=4")
+
+vim.keymap.set('n', '<C-c>', '"+y', { noremap = true})
+vim.keymap.set('v', '<C-c>', '"+y', { noremap = true})
+vim.keymap.set('i', '<C-c>', '<Esc>"+y`a', { noremap = true})
+
+vim.keymap.set('n', '<C-v>', '"+p', { noremap = true})
+vim.keymap.set('v', '<C-v>', '"+p', { noremap = true})
+vim.keymap.set('i', '<C-v>', '<Esc>"+p`a', { noremap = true})
+
+vim.keymap.set('n', '<C-z>', ':undo<CR>', { noremap = true})
+vim.keymap.set('v', '<C-z>', ':undo<CR>', { noremap = true})
+vim.keymap.set('i', '<C-z>', '<Esc>:undo<CR>i', { noremap = true})
+
+vim.api.nvim_set_keymap('n', '<S-Tab>', '<C-d>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<S-Tab>', '<C-d>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('i', '<S-Tab>', '<C-d>', {noremap = true, silent = true})
+
+vim.api.nvim_set_keymap('n', '<leader>t', ':vsplit | terminal<CR>', {noremap = true})
+vim.cmd('autocmd TermOpen * setlocal buftype=terminal')
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>:bd!<CR>', {noremap = true})
+vim.api.nvim_set_keymap('n', '<leader>w', ':bd!<CR>', {noremap = true})
+
+vim.api.nvim_set_keymap('i', '<C-f>', '<C-o>0', {noremap = true})
+
